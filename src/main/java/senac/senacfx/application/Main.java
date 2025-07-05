87f802f4635403f9bfaba450735362d467865264
ieliudj530 <eliud.gonzalez.530@gmail.com>
2025-07-05 10:25:47 -0300

    Update Main.java

--- src/main/java/senac/senacfx/application/Main.java
+++ src/main/java/senac/senacfx/application/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
 
             mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
-            primaryStage.setTitle("Titulo do seu sistema");
+            primaryStage.setTitle("Fabrica de Bolsos");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
